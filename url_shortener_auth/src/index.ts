ba6d4128a0d1ae88fc6c16605b776e0a49ee82c3
Rubem Moura <rubemfmourajr@gmail.com>
2024-02-11 22:30:55 -0600

    adding swagger api doc endpoint

--- url_shortener_auth/src/index.ts
+++ url_shortener_auth/src/index.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import authRoutes from './routes/authRoutes';
+import swaggerRouter from './routes/swaggerRoutes';
 
 require('dotenv').config();
 
 const app = express();
 app.use(express.json());
+app.use('/api', swaggerRouter);
 app.use('/auth', authRoutes);
 
 const port = 3000;
 app.listen(port, () => {
     console.log(`Authenticator is running at http://localhost:${port}`);
+    console.log(`API Docs is provided at http://localhost:${port}/api/swagger`);
 });
